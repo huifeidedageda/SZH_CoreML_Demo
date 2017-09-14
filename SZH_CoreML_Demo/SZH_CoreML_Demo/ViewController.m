32685105b8130f67ce569614f43eda7dd8032924
SongZhiheng <szh13598845390@126.com>
2017-09-14 15:02:15 +0800

    初始化了CoreML项目

---
+++ SZH_CoreML_Demo/SZH_CoreML_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SZH_CoreML_Demo
+//
+//  Created by 智衡宋 on 2017/9/14.
+//  Copyright © 2017年 智衡宋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
